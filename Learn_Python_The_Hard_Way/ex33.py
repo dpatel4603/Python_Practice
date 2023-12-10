12935996fcf1e457f66eba0774ac38a0e85d6c40
dpatel4603 <51136648+dpatel4603@users.noreply.github.com>
2023-12-09 19:30:29 -0500

    Updated Completed Examples 12-9-2023

---
+++ Learn_Python_The_Hard_Way/ex33.py
@@ -0,0 +1,18 @@
+
+i = 0
+numbers = []
+
+while i < 6:
+    print(f"At the top is i is {i}")
+    numbers.append(i)
+
+    i += 1
+    print("Numbers now: ", numbers)
+    print(f"At the bottom is {i}")
+
+print("The numbers: ")
+
+for num in numbers:
+    print(num)
+
+
